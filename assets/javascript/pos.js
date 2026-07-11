5a4650ee1980eba1d56b9664d58d44f999c59353
Areeba Noor <areebanoor5687@gmail.com>
2026-07-11 13:09:15 +0500

    feat : added POS data

---
+++ assets/javascript/pos.js
@@ -0,0 +1,87 @@
+"use strict";
+
+
+const POSData = {
+
+    categories: [
+
+        "All",
+        "Burger",
+        "Pizza",
+        "Chicken",
+        "Drinks",
+        "Dessert"
+
+    ],
+
+    customers: [
+
+        "Walk-in Customer",
+        "Areeba Noor",
+        "Ali Ahmed",
+        "Sarah Khan",
+        "John Smith"
+
+    ],
+
+    products: [
+
+        {
+            id: 1,
+            name: "Zinger Burger",
+            category: "Burger",
+            price: 8.50,
+            image: "assets/images/zingerBurger.jpg",
+            description: "Crispy chicken burger"
+        },
+
+        {
+            id: 2,
+            name: "Cheese Burger",
+            category: "Burger",
+            price: 9.50,
+            image: "assets/images/cheeseBurger.jpg",
+            description: "Double cheese burger"
+        },
+
+        {
+            id: 3,
+            name: "Chicken Pizza",
+            category: "Pizza",
+            price: 15,
+            image: "assests/images/chickenPizza.jpg",
+            description: "Large pizza"
+        },
+
+        {
+            id: 4,
+            name: "French Fries",
+            category: "Chicken",
+            price: 4,
+            image: "assets/images/frenchFries.jpg",
+            description: "Crispy fries"
+        },
+
+        {
+            id: 5,
+            name: "Cold Drink",
+            category: "Drinks",
+            price: 2,
+            image: "assets/images/coldDrink.jpg",
+            description: "Soft drink"
+        },
+
+        {
+            id: 6,
+            name: "Chocolate Cake",
+            category: "Dessert",
+            price: 6,
+            image: "assets/images/chocolateCake.jpg",
+            description: "Chocolate dessert"
+        }
+
+    ]
+
+};
+
+
